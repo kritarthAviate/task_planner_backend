21f87c661bcbedd0f3261fca531969bd7a6b2181
Alien Coder <paidcodecamp@gmail.com>
2023-02-25 23:31:03 +0530

    added cors and bodyparser

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 6000;
 
 const app = express();
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.NODE_ENV === "development" ? "*" : /domain\.com$/,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(PORT, function () {
   console.log(`Express app listening on port ${PORT}`);
