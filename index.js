ac62eae4d5a3bf1cffb9eab985d8a4fdbdf84394
Alien Coder <paidcodecamp@gmail.com>
2023-02-25 23:42:04 +0530

    added mongoose and connection with DB

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const PORT = process.env.PORT || 6000;
 
 const app = express();
+
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster.6llo7lr.mongodb.net/TaskPlanner?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(mongoString);
+
+mongoose.connection.on("error", function (error) {
+  if (process.env.NODE_ENV === "development") {
+    console.log(error);
+  }
+});
+
+mongoose.connection.on("open", function () {
+  console.log("Connected to MongoDB database.");
+});
 
 app.use(helmet());
 
