b60277bf0748a5fc5866706520111e80ec30e534
Alien Coder <paidcodecamp@gmail.com>
2023-03-01 11:32:14 +0530

    added cookieParser

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const { connectDB } = require("./db/mongoose");
 const userRouter = require("./routers/user");
@@ -14,6 +15,8 @@ connectDB();
 app.use(helmet());
 
 app.use(cors());
+
+app.use(cookieParser());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
